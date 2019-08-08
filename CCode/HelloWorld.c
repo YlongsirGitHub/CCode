f0082d79114dd45d99f9fd1527f3e76e78e184f6
ylong_qin <ylong_qin@outlook.com>
2019-08-08 16:47:45 +0800

    删除本地的TestMode仓库

--- CCode/HelloWorld.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <windows.h>
-void main()
-{
-	printf("Hello World!\n");
-	system("pause");
-}
-
-
--- CCode/test.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <windows.h>
-void main()
-{
-	int a[3], b, c;
-	printf("%d", a[3]);
-	printf("pause");
-}
